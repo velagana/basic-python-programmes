5e42c4f8f0850f533777d036a63ddfed625e8fdf
Anusha <116190024+velagana@users.noreply.github.com>
2024-03-04 14:55:21 +0530

    Add files via upload

---
+++ global varible.py
@@ -0,0 +1,10 @@
+#Global varible 
+mohith_love ="Anusha"
+def Mohith():
+	anusha_love ="mohith"
+	anusha_mohith ="love"
+	print("anusha_love")
+	print("anusha_mohith")
+Mohith()
+print("mohith_love")
+print("anusha_love")
