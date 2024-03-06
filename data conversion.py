6010cfb0bdbf284e6dd97df83c5483259b494e50
Anusha <116190024+velagana@users.noreply.github.com>
2024-03-06 13:15:39 +0530

    Add files via upload

---
+++ data conversion.py
@@ -0,0 +1,7 @@
+#data conversion : converting one type of data type into other  data types is called the data conversion
+a = 10;
+b = complex(a)
+print(b)
+f = 29
+g = float(f)
+print(g)
---
+++ numbers.py
@@ -0,0 +1,19 @@
+#Number
+a = 1
+b = 2
+c = 10
+print(type(a))
+print(type(b))
+print(type(c))
+d = 20.0
+e = 40.34
+f = 69.83
+print(type(d))
+print(type(e))
+print(type(f))
+g = 2+3j
+h = 3+6J
+i = -7+10J
+print(type(g))
+print(type(h))
+print(type(i))
